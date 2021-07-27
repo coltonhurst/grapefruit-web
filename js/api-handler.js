8e50a0f614657025da281399cd9972b5c291200c
Colton Hurst <colton@coltonhurst.com>
2021-07-26 22:13:31 -0400

    Add all routes to api-handler

--- js/api-handler.js
+++ js/api-handler.js
@@ -10,9 +10,61 @@
 
     If the API returns an HTTP status of 401 or 403, the logout()
     function will be called, forcing the user to sign in again.
+
+    The following are supported below:
+    /member
+      POST   | create a member | signup()
+      GET    | member login    | login()
+      PUT    | update a member | updateMember()
+
+    /posts
+      POST   | create a post   | createPost()
+      PUT    | update a post   | updatePost()
+      GET    | get posts       | getPosts()
+      DELETE | delete a post   | deletePost()
+
+    /comments
+      POST   | create a comment| createComment()
+      PUT    | update a comment| updateComment()
+      GET    | get comments    | getComments()
+      DELETE | delete a post   | deleteComment()
+
+    /likepost
+      POST   | like a post     | likeAPost()
+
+    /likecomment
+      POST   | like a comment  | likeAComment()
 */
 
 const API_URL_V1 = 'http://127.0.0.1:8000/v1/';
+
+/* Facilitates signup */
+function signup(username, email, password, successFunc, failureFunc) {
+    const url = API_URL_V1 + 'member';
+    const authHeader = "Basic " + btoa(email + ":" + password);
+    const body = {
+        email: email,
+        username: username
+    };
+
+    const options = {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': authHeader
+        }
+    };
+
+    fetch(url, options).then(function (response) {
+        // success
+        localStorage.setItem("Authorization", authHeader);
+        successFunc(response.json());
+    }).catch(function (err) {
+        // failure
+        failureFunc(err);
+    });
+}
 
 /* Facilitates login */
 function login(email, password, successFunc, failureFunc) {
@@ -37,17 +89,17 @@ function login(email, password, successFunc, failureFunc) {
     });
 }
 
-/* Facilitates signup */
-function signup(username, email, password, successFunc, failureFunc) {
+/* Facilitates updating member email and password */
+function updateMember(newEmail, newPassword, successFunc, failureFunc) {
     const url = API_URL_V1 + 'member';
+    const authHeader = localStorage("Authorization");
     const body = {
-        email: email,
-        username: username
+        email: newEmail,
+        password: btoa(newPassword)
     };
-    const authHeader = "Basic " + btoa(email + ":" + password);
 
     const options = {
-        method: 'POST',
+        method: 'PUT',
         body: JSON.stringify(body),
         headers: {
             'Content-Type': 'application/json',
@@ -57,7 +109,7 @@ function signup(username, email, password, successFunc, failureFunc) {
 
     fetch(url, options).then(function (response) {
         // success
-        localStorage.setItem("Authorization", authHeader);
+        localStorage.setItem("Authorization", btoa("Basic " + newEmail + ":" + newPassword));
         successFunc(response.json());
     }).catch(function (err) {
         // failure
@@ -69,6 +121,61 @@ function signup(username, email, password, successFunc, failureFunc) {
 function logout() {
     localStorage.removeItem("Authorization");
     window.location = "/index.html";
+}
+
+/* Facilitates creating a post */
+function createPost(postTitle, postBody, successFunc, failureFunc) {
+    const url = API_URL_V1 + 'posts';
+    const authHeader = localStorage.getItem("Authorization");
+    const body = {
+        title: postTitle,
+        body: postBody,
+        author: atob(authHeader).split(';')[1]
+    };
+
+    const options = {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': authHeader
+        }
+    };
+
+    fetch(url).then(function (response) {
+        // success
+        successFunc(response.json());
+    }).catch(function (err) {
+        // failure
+        failureFunc(err);
+    });
+}
+
+/* Facilitates updating a post */
+function updatePost(postGuid, newPostBody, successFunc, failureFunc) {
+    const url = API_URL_V1 + 'posts';
+    const authHeader = localStorage.getItem("Authorization");
+    const body = {
+        id: postGuid,
+        body: newPostBody
+    };
+
+    const options = {
+        method: 'PUT',
+        body: JSON.stringify(body),
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': authHeader
+        }
+    };
+
+    fetch(url).then(function (response) {
+        // success
+        successFunc(response.json());
+    }).catch(function (err) {
+        // failure
+        failureFunc(err);
+    });
 }
 
 /* Facilitates getting the posts */
@@ -84,4 +191,201 @@ function getPosts(successFunc, failureFunc) {
     });
 }
 
-export { login, signup, getPosts };
+/* Facilitates deleting a post */
+function deletePost(postGuid, successFunc, failureFunc) {
+    const url = API_URL_V1 + 'posts';
+    const authHeader = localStorage.getItem("Authorization");
+    const body = {
+        id: postGuid
+    };
+
+    const options = {
+        method: 'DELETE',
+        body: JSON.stringify(body),
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': authHeader
+        }
+    };
+
+    fetch(url).then(function (response) {
+        // success
+        successFunc(response.json());
+    }).catch(function (err) {
+        // failure
+        failureFunc(err);
+    });
+}
+
+/* Facilitates creating a post */
+function createComment(commentBody, successFunc, failureFunc) {
+    const url = API_URL_V1 + 'comments';
+    const authHeader = localStorage.getItem("Authorization");
+    const body = {
+        body: commentBody,
+        author: atob(authHeader).split(';')[1]
+    };
+
+    const options = {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': authHeader
+        }
+    };
+
+    fetch(url).then(function (response) {
+        // success
+        successFunc(response.json());
+    }).catch(function (err) {
+        // failure
+        failureFunc(err);
+    });
+}
+
+/* Facilitates updating a post */
+function updateComment(commentGuid, newCommentBody, successFunc, failureFunc) {
+    const url = API_URL_V1 + 'comments';
+    const authHeader = localStorage.getItem("Authorization");
+    const body = {
+        id: commentGuid,
+        body: newCommentBody
+    };
+
+    const options = {
+        method: 'PUT',
+        body: JSON.stringify(body),
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': authHeader
+        }
+    };
+
+    fetch(url).then(function (response) {
+        // success
+        successFunc(response.json());
+    }).catch(function (err) {
+        // failure
+        failureFunc(err);
+    });
+}
+
+/* Facilitates getting the comments for a set of posts */
+function getComments(postGuids, successFunc, failureFunc) {
+    const url = API_URL_V1 + 'comments';
+    const body = {
+        postIds: postGuids
+    };
+
+    const options = {
+        method: 'GET',
+        body: JSON.stringify(body),
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': authHeader
+        }
+    };
+
+    fetch(url).then(function (response) {
+        // success
+        successFunc(response.json());
+    }).catch(function (err) {
+        // failure
+        failureFunc(err);
+    });
+}
+
+/* Facilitates deleting a post */
+function deleteComment(commentGuid, successFunc, failureFunc) {
+    const url = API_URL_V1 + 'comments';
+    const authHeader = localStorage.getItem("Authorization");
+    const body = {
+        id: commentGuid
+    };
+
+    const options = {
+        method: 'DELETE',
+        body: JSON.stringify(body),
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': authHeader
+        }
+    };
+
+    fetch(url).then(function (response) {
+        // success
+        successFunc(response.json());
+    }).catch(function (err) {
+        // failure
+        failureFunc(err);
+    });
+}
+
+/* Facilitates creating a post */
+function likeAPost(postGuid, successFunc, failureFunc) {
+    const url = API_URL_V1 + 'likepost';
+    const authHeader = localStorage.getItem("Authorization");
+    const body = {
+        id: postGuid
+    };
+
+    const options = {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': authHeader
+        }
+    };
+
+    fetch(url).then(function (response) {
+        // success
+        successFunc(response.json());
+    }).catch(function (err) {
+        // failure
+        failureFunc(err);
+    });
+}
+
+/* Facilitates creating a post */
+function likeAComment(commentGuid, successFunc, failureFunc) {
+    const url = API_URL_V1 + 'likecomment';
+    const authHeader = localStorage.getItem("Authorization");
+    const body = {
+        id: commentGuid
+    };
+
+    const options = {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': authHeader
+        }
+    };
+
+    fetch(url).then(function (response) {
+        // success
+        successFunc(response.json());
+    }).catch(function (err) {
+        // failure
+        failureFunc(err);
+    });
+}
+
+export {
+    signup,
+    login,
+    updateMember,
+    createPost,
+    updatePost,
+    getPosts,
+    deletePost,
+    createComment,
+    updateComment,
+    getComments,
+    deleteComment,
+    likeAPost,
+    likeAComment
+};
