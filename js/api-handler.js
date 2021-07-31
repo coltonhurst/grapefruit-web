9addbd0774e025090027002ff0d445d29ab30fe7
coltonhurst <colton@coltonhurst.com>
2021-07-31 00:01:05 -0400

    Support login

--- js/api-handler.js
+++ js/api-handler.js
@@ -40,352 +40,359 @@ const API_URL_V1 = 'http://127.0.0.1:8000/v1/';
 
 /* Facilitates signup */
 function signup(username, email, password, successFunc, failureFunc) {
-    const url = API_URL_V1 + 'member';
-    const authHeader = "Basic " + btoa(email + ":" + password);
-    const body = {
-        email: email,
-        username: username
-    };
+  const url = API_URL_V1 + 'member';
+  const authHeader = "Basic " + btoa(email + ":" + password);
+  const body = {
+    email: email,
+    username: username
+  };
 
-    const options = {
-        method: 'POST',
-        body: JSON.stringify(body),
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': authHeader
-        }
-    };
+  const options = {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': authHeader
+    }
+  };
 
-    fetch(url, options).then(function (response) {
-        // success
-        localStorage.setItem("Authorization", authHeader);
-        successFunc(response.json());
-    }).catch(function (err) {
-        // failure
-        failureFunc(err);
-    });
+  fetch(url, options).then(function (response) {
+    // success
+    localStorage.setItem("Authorization", authHeader);
+    successFunc(response.json());
+  }).catch(function (err) {
+    // failure
+    failureFunc(err);
+  });
 }
 
 /* Facilitates login */
-function login(email, password, successFunc, failureFunc) {
-    const url = API_URL_V1 + 'member';
-    const authHeader = "Basic " + btoa(email + ":" + password);
+function login(email, password, actionFunc) {
+  const url = API_URL_V1 + 'login';
+  const authHeader = btoa(email + ":" + password);
+  const body = {
+    authorization: authHeader,
+    email: email
+  };
 
-    const options = {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': authHeader
-        }
-    };
+  const options = {
+    method: 'POST',
+    body: JSON.stringify(body),
+    /*headers: {
+        'Content-Type': 'application/json',
+        'Authorization': authHeader
+    }*/
+  };
 
-    fetch(url, options).then(function (response) {
-        // success
-        localStorage.setItem("Authorization", authHeader);
-        successFunc(response.json());
-    }).catch(function (err) {
-        // failure
-        failureFunc(err);
-    });
+  fetch(url, options).then(function (response) {
+    // success
+    localStorage.setItem("Authorization", authHeader);
+    return response.json();
+  }).then((response) => {
+    actionFunc(response)
+  }).catch(function (err) {
+    // failure
+    console.log(err);
+  });
 }
 
 /* Facilitates updating member email and password */
 function updateMember(newEmail, newPassword, successFunc, failureFunc) {
-    const url = API_URL_V1 + 'member';
-    const authHeader = localStorage("Authorization");
-    const body = {
-        email: newEmail,
-        password: btoa(newPassword)
-    };
+  const url = API_URL_V1 + 'member';
+  const authHeader = localStorage("Authorization");
+  const body = {
+    email: newEmail,
+    password: btoa(newPassword)
+  };
 
-    const options = {
-        method: 'PUT',
-        body: JSON.stringify(body),
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': authHeader
-        }
-    };
+  const options = {
+    method: 'PUT',
+    body: JSON.stringify(body),
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': authHeader
+    }
+  };
 
-    fetch(url, options).then(function (response) {
-        // success
-        localStorage.setItem("Authorization", btoa("Basic " + newEmail + ":" + newPassword));
-        successFunc(response.json());
-    }).catch(function (err) {
-        // failure
-        failureFunc(err);
-    });
+  fetch(url, options).then(function (response) {
+    // success
+    localStorage.setItem("Authorization", btoa("Basic " + newEmail + ":" + newPassword));
+    successFunc(response.json());
+  }).catch(function (err) {
+    // failure
+    failureFunc(err);
+  });
 }
 
 /* Facilitates logout */
 function logout() {
-    localStorage.removeItem("Authorization");
-    window.location = "/index.html";
+  localStorage.removeItem("Authorization");
+  window.location = "/index.html";
 }
 
 /* Facilitates creating a post */
 function createPost(postTitle, postBody, successFunc, failureFunc) {
-    const url = API_URL_V1 + 'posts';
-    const authHeader = localStorage.getItem("Authorization");
-    const body = {
-        title: postTitle,
-        body: postBody,
-        author: atob(authHeader).split(';')[1]
-    };
+  const url = API_URL_V1 + 'posts';
+  const authHeader = localStorage.getItem("Authorization");
+  const body = {
+    title: postTitle,
+    body: postBody,
+    author: atob(authHeader).split(';')[1]
+  };
 
-    const options = {
-        method: 'POST',
-        body: JSON.stringify(body),
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': authHeader
-        }
-    };
+  const options = {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': authHeader
+    }
+  };
 
-    fetch(url).then(function (response) {
-        // success
-        successFunc(response.json());
-    }).catch(function (err) {
-        // failure
-        failureFunc(err);
-    });
+  fetch(url).then(function (response) {
+    // success
+    successFunc(response.json());
+  }).catch(function (err) {
+    // failure
+    failureFunc(err);
+  });
 }
 
 /* Facilitates updating a post */
 function updatePost(postGuid, newPostBody, successFunc, failureFunc) {
-    const url = API_URL_V1 + 'posts';
-    const authHeader = localStorage.getItem("Authorization");
-    const body = {
-        id: postGuid,
-        body: newPostBody
-    };
+  const url = API_URL_V1 + 'posts';
+  const authHeader = localStorage.getItem("Authorization");
+  const body = {
+    id: postGuid,
+    body: newPostBody
+  };
 
-    const options = {
-        method: 'PUT',
-        body: JSON.stringify(body),
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': authHeader
-        }
-    };
+  const options = {
+    method: 'PUT',
+    body: JSON.stringify(body),
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': authHeader
+    }
+  };
 
-    fetch(url).then(function (response) {
-        // success
-        successFunc(response.json());
-    }).catch(function (err) {
-        // failure
-        failureFunc(err);
-    });
+  fetch(url).then(function (response) {
+    // success
+    successFunc(response.json());
+  }).catch(function (err) {
+    // failure
+    failureFunc(err);
+  });
 }
 
 /* Facilitates getting the posts */
 function getPosts(successFunc, failureFunc) {
-    const url = API_URL_V1 + 'posts';
+  const url = API_URL_V1 + 'posts';
 
-    fetch(url).then(function (response) {
-        // success
-        successFunc(response.json());
-    }).catch(function (err) {
-        // failure
-        failureFunc(err);
-    });
+  fetch(url).then(function (response) {
+    // success
+    successFunc(response.json());
+  }).catch(function (err) {
+    // failure
+    failureFunc(err);
+  });
 }
 
 /* Facilitates deleting a post */
 function deletePost(postGuid, successFunc, failureFunc) {
-    const url = API_URL_V1 + 'posts';
-    const authHeader = localStorage.getItem("Authorization");
-    const body = {
-        id: postGuid
-    };
+  const url = API_URL_V1 + 'posts';
+  const authHeader = localStorage.getItem("Authorization");
+  const body = {
+    id: postGuid
+  };
 
-    const options = {
-        method: 'DELETE',
-        body: JSON.stringify(body),
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': authHeader
-        }
-    };
+  const options = {
+    method: 'DELETE',
+    body: JSON.stringify(body),
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': authHeader
+    }
+  };
 
-    fetch(url).then(function (response) {
-        // success
-        successFunc(response.json());
-    }).catch(function (err) {
-        // failure
-        failureFunc(err);
-    });
+  fetch(url).then(function (response) {
+    // success
+    successFunc(response.json());
+  }).catch(function (err) {
+    // failure
+    failureFunc(err);
+  });
 }
 
 /* Facilitates creating a comment */
 function createComment(commentBody, successFunc, failureFunc) {
-    const url = API_URL_V1 + 'comments';
-    const authHeader = localStorage.getItem("Authorization");
-    const body = {
-        body: commentBody,
-        author: atob(authHeader).split(';')[1]
-    };
+  const url = API_URL_V1 + 'comments';
+  const authHeader = localStorage.getItem("Authorization");
+  const body = {
+    body: commentBody,
+    author: atob(authHeader).split(';')[1]
+  };
 
-    const options = {
-        method: 'POST',
-        body: JSON.stringify(body),
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': authHeader
-        }
-    };
+  const options = {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': authHeader
+    }
+  };
 
-    fetch(url).then(function (response) {
-        // success
-        successFunc(response.json());
-    }).catch(function (err) {
-        // failure
-        failureFunc(err);
-    });
+  fetch(url).then(function (response) {
+    // success
+    successFunc(response.json());
+  }).catch(function (err) {
+    // failure
+    failureFunc(err);
+  });
 }
 
 /* Facilitates updating a comment */
 function updateComment(commentGuid, newCommentBody, successFunc, failureFunc) {
-    const url = API_URL_V1 + 'comments';
-    const authHeader = localStorage.getItem("Authorization");
-    const body = {
-        id: commentGuid,
-        body: newCommentBody
-    };
+  const url = API_URL_V1 + 'comments';
+  const authHeader = localStorage.getItem("Authorization");
+  const body = {
+    id: commentGuid,
+    body: newCommentBody
+  };
 
-    const options = {
-        method: 'PUT',
-        body: JSON.stringify(body),
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': authHeader
-        }
-    };
+  const options = {
+    method: 'PUT',
+    body: JSON.stringify(body),
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': authHeader
+    }
+  };
 
-    fetch(url).then(function (response) {
-        // success
-        successFunc(response.json());
-    }).catch(function (err) {
-        // failure
-        failureFunc(err);
-    });
+  fetch(url).then(function (response) {
+    // success
+    successFunc(response.json());
+  }).catch(function (err) {
+    // failure
+    failureFunc(err);
+  });
 }
 
 /* Facilitates getting the comments for a set of posts */
 function getComments(postGuids, successFunc, failureFunc) {
-    const url = API_URL_V1 + 'comments';
-    const body = {
-        postIds: postGuids
-    };
+  const url = API_URL_V1 + 'comments';
+  const body = {
+    postIds: postGuids
+  };
 
-    const options = {
-        method: 'GET',
-        body: JSON.stringify(body),
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': authHeader
-        }
-    };
+  const options = {
+    method: 'GET',
+    body: JSON.stringify(body),
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': authHeader
+    }
+  };
 
-    fetch(url).then(function (response) {
-        // success
-        successFunc(response.json());
-    }).catch(function (err) {
-        // failure
-        failureFunc(err);
-    });
+  fetch(url).then(function (response) {
+    // success
+    successFunc(response.json());
+  }).catch(function (err) {
+    // failure
+    failureFunc(err);
+  });
 }
 
 /* Facilitates deleting a comment */
 function deleteComment(commentGuid, successFunc, failureFunc) {
-    const url = API_URL_V1 + 'comments';
-    const authHeader = localStorage.getItem("Authorization");
-    const body = {
-        id: commentGuid
-    };
+  const url = API_URL_V1 + 'comments';
+  const authHeader = localStorage.getItem("Authorization");
+  const body = {
+    id: commentGuid
+  };
 
-    const options = {
-        method: 'DELETE',
-        body: JSON.stringify(body),
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': authHeader
-        }
-    };
+  const options = {
+    method: 'DELETE',
+    body: JSON.stringify(body),
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': authHeader
+    }
+  };
 
-    fetch(url).then(function (response) {
-        // success
-        successFunc(response.json());
-    }).catch(function (err) {
-        // failure
-        failureFunc(err);
-    });
+  fetch(url).then(function (response) {
+    // success
+    successFunc(response.json());
+  }).catch(function (err) {
+    // failure
+    failureFunc(err);
+  });
 }
 
 /* Facilitates creating a post */
 function likeAPost(postGuid, successFunc, failureFunc) {
-    const url = API_URL_V1 + 'likepost';
-    const authHeader = localStorage.getItem("Authorization");
-    const body = {
-        id: postGuid
-    };
+  const url = API_URL_V1 + 'likepost';
+  const authHeader = localStorage.getItem("Authorization");
+  const body = {
+    id: postGuid
+  };
 
-    const options = {
-        method: 'POST',
-        body: JSON.stringify(body),
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': authHeader
-        }
-    };
+  const options = {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': authHeader
+    }
+  };
 
-    fetch(url).then(function (response) {
-        // success
-        successFunc(response.json());
-    }).catch(function (err) {
-        // failure
-        failureFunc(err);
-    });
+  fetch(url).then(function (response) {
+    // success
+    successFunc(response.json());
+  }).catch(function (err) {
+    // failure
+    failureFunc(err);
+  });
 }
 
 /* Facilitates creating a post */
 function likeAComment(commentGuid, successFunc, failureFunc) {
-    const url = API_URL_V1 + 'likecomment';
-    const authHeader = localStorage.getItem("Authorization");
-    const body = {
-        id: commentGuid
-    };
+  const url = API_URL_V1 + 'likecomment';
+  const authHeader = localStorage.getItem("Authorization");
+  const body = {
+    id: commentGuid
+  };
 
-    const options = {
-        method: 'POST',
-        body: JSON.stringify(body),
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': authHeader
-        }
-    };
+  const options = {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': authHeader
+    }
+  };
 
-    fetch(url).then(function (response) {
-        // success
-        successFunc(response.json());
-    }).catch(function (err) {
-        // failure
-        failureFunc(err);
-    });
+  fetch(url).then(function (response) {
+    // success
+    successFunc(response.json());
+  }).catch(function (err) {
+    // failure
+    failureFunc(err);
+  });
 }
 
 export {
-    signup,
-    login,
-    updateMember,
-    createPost,
-    updatePost,
-    getPosts,
-    deletePost,
-    createComment,
-    updateComment,
-    getComments,
-    deleteComment,
-    likeAPost,
-    likeAComment
+  signup,
+  login,
+  updateMember,
+  createPost,
+  updatePost,
+  getPosts,
+  deletePost,
+  createComment,
+  updateComment,
+  getComments,
+  deleteComment,
+  likeAPost,
+  likeAComment
 };
--- js/index.js
+++ js/index.js
@@ -29,8 +29,8 @@ clearFormData();
 document.getElementById("login-modal-btn").addEventListener("click", login);
 document.getElementById("signup-modal-btn").addEventListener("click", signup);
 document.getElementById("login-modal-email").oninput = function () {
-    if (!loginFormIsErrorFree)
-        hideLoginError();
+  if (!loginFormIsErrorFree)
+    hideLoginError();
 };
 //document.getElementById("login-btn").addEventListener("click", loginFocus);
 //document.getElementById("signup-btn").addEventListener("click", signupFocus);
@@ -40,24 +40,24 @@ document.getElementById("login-modal-email").oninput = function () {
     clear the errors, only if an error is already showing.
 */
 document.getElementById("login-modal-password").oninput = function () {
-    if (!loginFormIsErrorFree)
-        hideLoginError();
+  if (!loginFormIsErrorFree)
+    hideLoginError();
 };
 document.getElementById("signup-modal-username").oninput = function () {
-    if (!signupFormIsErrorFree)
-        hideLoginError();
+  if (!signupFormIsErrorFree)
+    hideLoginError();
 };
 document.getElementById("signup-modal-username").oninput = function () {
-    if (!signupFormIsErrorFree)
-        hideSignupError();
+  if (!signupFormIsErrorFree)
+    hideSignupError();
 };
 document.getElementById("signup-modal-email").oninput = function () {
-    if (!signupFormIsErrorFree)
-        hideSignupError();
+  if (!signupFormIsErrorFree)
+    hideSignupError();
 };
 document.getElementById("signup-modal-password").oninput = function () {
-    if (!signupFormIsErrorFree)
-        hideSignupError();
+  if (!signupFormIsErrorFree)
+    hideSignupError();
 };
 
 /*
@@ -68,107 +68,106 @@ Functions
 
 /* Give the login email focus */
 function loginFocus() {
-    // todo: implement me
+  // todo: implement me
 }
 
 /* Give the signup email focus */
 function signupFocus() {
-    // todo: implement me
+  // todo: implement me
 }
 
 /* Login attempt */
 function login() {
-    if (!validateLoginForm()) {
-        return;
+  if (!validateLoginForm()) {
+    return;
+  }
+
+  const email = document.getElementById("login-modal-email").value.trim();
+  const password = document.getElementById("login-modal-password").value;
+
+  apiHandler.login(email, password, (data) => {
+    if (isNullOrWhitespace(data.error)) {
+      window.location = "/account.html";
+    } else {
+      showLoginError(data.error);
     }
-
-    const email = document.getElementById("login-modal-email").value.trim();
-    const password = document.getElementById("login-modal-password").value;
-
-    apiHandler.login(email, password, (data) => {
-        // on success
-        console.log("login success!", data);
-        window.location = "/account.html";
-    }, (err) => {
-        // on failure
-        console.log("login error!", err);
-    });
+  });
 }
 
 /* Signup attempt */
 function signup() {
 
-    if (!validateSignupForm()) {
-        return;
-    }
+  if (!validateSignupForm()) {
+    return;
+  }
 
-    const username = document.getElementById("signup-modal-username").value.trim();
-    const email = document.getElementById("signup-modal-email").value.trim().toLowerCase();
-    const password = document.getElementById("signup-modal-password").value;
+  const username = document.getElementById("signup-modal-username").value.trim();
+  const email = document.getElementById("signup-modal-email").value.trim().toLowerCase();
+  const password = document.getElementById("signup-modal-password").value;
 
-    apiHandler.signup(username, email, password, (data) => {
-        // on success
-        console.log("signup success!", data);
-        window.location = "/account.html";
-    }, (err) => {
-        // on failure
-        console.log("signup error!", err);
-    });
+  apiHandler.signup(username, email, password, (data) => {
+    // on success
+    console.log("signup success!", data);
+    window.location = "/account.html";
+  }, (err) => {
+    // on failure
+    console.log("signup error!", err);
+  });
 }
 
 /* Validate the login form */
 function validateLoginForm() {
-    const email = document.getElementById("login-modal-email").value.trim();
-    const password = document.getElementById("login-modal-password").value;
+  const email = document.getElementById("login-modal-email").value.trim();
+  const password = document.getElementById("login-modal-password").value;
 
-    if (!validateEmail(email)) {
-        showLoginError("Please enter a valid email address.");
-        return false;
-    } else if (password.length == 0) {
-        showLoginError("Please enter a password.");
-        return false;
-    }
+  if (!validateEmail(email)) {
+    showLoginError("Please enter a valid email address.");
+    return false;
+  } else if (password.length == 0) {
+    showLoginError("Please enter a password.");
+    return false;
+  }
 
-    hideLoginError();
+  hideLoginError();
 
-    return true;
+  return true;
 }
 
 /* Validate the signup form */
 function validateSignupForm() {
-    const username = document.getElementById("signup-modal-username").value.trim();
-    const email = document.getElementById("signup-modal-email").value.trim();
-    const password = document.getElementById("signup-modal-password").value;
+  const username = document.getElementById("signup-modal-username").value.trim();
+  const email = document.getElementById("signup-modal-email").value.trim();
+  const password = document.getElementById("signup-modal-password").value;
 
-    if (username.length <= 3) {
-        showSignupError("Username must be longer than 3 characters.");
-        return false;
-    } else if (!validateEmail(email)) {
-        showSignupError("Please enter a valid email address.");
-        return false;
-    } else if (password.length == 0) {
-        showSignupError("Please enter a password.");
-        return false;
-    }
+  if (username.length <= 3) {
+    showSignupError("Username must be longer than 3 characters.");
+    return false;
+  } else if (!validateEmail(email)) {
+    showSignupError("Please enter a valid email address.");
+    return false;
+  } else if (password.length == 0) {
+    showSignupError("Please enter a password.");
+    return false;
+  }
 
-    hideSignupError();
+  hideSignupError();
 
-    return true;
+  return true;
 }
 
 /* Validate the email */
 function validateEmail(email) {
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
 }
 
 /* Clear the data in the login and signup modals */
 function clearFormData() {
-    document.getElementById("login-modal-email").value = "";
-    document.getElementById("login-modal-password").value = "";
-    document.getElementById("signup-modal-username").value = "";
-    document.getElementById("signup-modal-email").value = "";
-    document.getElementById("signup-modal-password").value = "";
+  document.getElementById("login-modal-email").value = "";
+  document.getElementById("login-modal-password").value = "";
+  document.getElementById("signup-modal-username").value = "";
+  document.getElementById("signup-modal-email").value = "";
+  document.getElementById("signup-modal-password").value = "";
 }
 
 /*
@@ -176,26 +175,33 @@ function clearFormData() {
     on the login and signup modals.
 */
 function showLoginError(errorMessage) {
-    let loginModalError = document.getElementById("login-modal-error");
-    loginModalError.innerHTML = errorMessage;
-    loginModalError.style.visibility = 'visible';
-    loginFormIsErrorFree = false;
+  let loginModalError = document.getElementById("login-modal-error");
+  loginModalError.innerHTML = errorMessage;
+  loginModalError.style.visibility = 'visible';
+  loginFormIsErrorFree = false;
 }
 function hideLoginError() {
-    let loginModalError = document.getElementById("login-modal-error");
-    loginModalError.innerHTML = "";
-    loginModalError.style.visibility = 'hidden';
-    loginFormIsErrorFree = true;
+  let loginModalError = document.getElementById("login-modal-error");
+  loginModalError.innerHTML = "";
+  loginModalError.style.visibility = 'hidden';
+  loginFormIsErrorFree = true;
 }
 function showSignupError(errorMessage) {
-    let signupModalError = document.getElementById("signup-modal-error");
-    signupModalError.innerHTML = errorMessage;
-    signupModalError.style.visibility = 'visible';
-    signupFormIsErrorFree = false;
+  let signupModalError = document.getElementById("signup-modal-error");
+  signupModalError.innerHTML = errorMessage;
+  signupModalError.style.visibility = 'visible';
+  signupFormIsErrorFree = false;
 }
 function hideSignupError() {
-    let signupModalError = document.getElementById("signup-modal-error");
-    signupModalError.innerHTML = "";
-    signupModalError.style.visibility = 'hidden';
-    signupFormIsErrorFree = false;
+  let signupModalError = document.getElementById("signup-modal-error");
+  signupModalError.innerHTML = "";
+  signupModalError.style.visibility = 'hidden';
+  signupFormIsErrorFree = false;
+}
+
+/*
+  Returns true if data is undefined, null, or whitespace.
+*/
+function isNullOrWhitespace(data) {
+  return data == undefined || data == null || data.trim() == "";
 }
